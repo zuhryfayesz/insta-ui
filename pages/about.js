8f5102e679fa8b8c7c9c834314e0395bcde0b6e4
zuhryhrc <zuhryhrc@bitbucket.org>
2021-03-05 00:56:26 +0530

    initial

--- pages/about.js
+++ pages/about.js
@@ -3,15 +3,9 @@ import Head from 'next/head'
 const About = () => {
     return (
         <>
-            <Head>
-                <title> Ninja List | About </title>
-                <meta name="keywords" content="ninjas" />
-            </Head>
-            <div>
                 <h1>About</h1>
                 <p>lorem ipsum dolor sit amet, consectetur adip</p>
                 <p>lorem ipsum dolor sit amet, consectetur adip</p>
-            </div>
         </>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,20 @@ import styles from '../styles/Home.module.css'
 import Navbar from '../comps/Navbar'
 import Footer from '../comps/Footer'
 import Link from 'next/link'
+import {useEffect} from 'react'
+import {useRouter} from 'next/router'
 
 export default function Home() {
+
+  const router = useRouter()
+
+  useEffect(() => {
+      router.push('/posts/')
+  }, [])
+
   return (
     <>
-    <Head>
-      <title> Ninja List | Home </title>
-      <meta name="keywords" content="ninjas"/>
-    </Head>
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>lorem ipsum dolor sit amet, consectetur adip</p>
-      <p className={styles.text}>lorem ipsum dolor sit amet, consectetur adip</p>
-      <Link href="/ninjas">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+      <div>Loading ...</div>
     </>
   )
 }
